d278154fdebcb04496744af0a66ad7ee8717c741
GaryGSC <GaryGSC@users.noreply.github.com>
2020-05-20 16:29:04 -0600

    Squash about 20 commits to make this initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# ![BYU logo](https://www.hscripts.com/freeimages/logos/university-logos/byu/byu-logo-clipart-128.gif) disallow-concurrent-runs
+A GitHub Action for disallowing concurrent workflow runs
+
+Unfortunately, GitHub Actions does not currently have a way to queue workflows.
+
+If you have a workflow to, say, deploy an application and it goes :boom: if multiple deployments are changing things at the same time, then this might be for you.
+
+Simply, this fails if the given workflow is already running.
+
+## Usage
+
+```yaml
+# ...
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Disallow Concurrent Runs
+        uses: byu-oit/disallow-concurrent-runs@v1
+        with:
+          token: ${{ github.token }}
+      # ... The rest of your deployment
+```
+
+## Contributing
+Hopefully this is useful to others at BYU. Feel free to ask me some questions about it, but I make no promises about being able to commit time to support it.
+
+### Modifying Source Code
+
+Just run `npm install` locally. There aren't many files here, so hopefully it should be pretty straightforward.
+
+### Cutting new releases
+
+GitHub Actions will run the entry point from the `action.yml`. In our case, that happens to be `/dist/index.js`.
+
+Actions run from GitHub repos. We don't want to check in `node_modules`. Hence, we package the app using `npm run package`.
+
+Then, be sure to create a new GitHub release, following SemVer.
---
+++ index.js
@@ -0,0 +1,71 @@
+const { getInput, startGroup, endGroup, setFailed } = require('@actions/core')
+const github = require('@actions/github')
+
+async function run () {
+  try {
+    const token = getInput('token', { required: true })
+    const octokit = new github.GitHub(token)
+
+    const { repo: { owner, repo }, workflow: workflowName, payload: { ref }, sha } = github.context
+    const branch = ref.slice('refs/heads/'.length) // ref = 'refs/heads/master'
+
+    const { data: { workflows } } = await octokit.actions.listRepoWorkflows({
+      owner,
+      repo
+    })
+    const { id: workflowId, path: pathToWorkflow } = workflows.find(workflow => workflow.name === workflowName && workflow.state === 'active')
+
+    startGroup('Workflow Info')
+    console.log({ owner, repo, branch, workflowName, workflowId, pathToWorkflow })
+    endGroup()
+
+    const { data: { workflow_runs: workflowRuns } } = await octokit.actions.listWorkflowRuns({
+      owner,
+      repo,
+      workflow_id: workflowId,
+      branch
+    })
+    const runs = workflowRuns.map(run => ({
+      runNumber: run.run_number,
+      commit: {
+        message: run.head_commit.message,
+        author: run.head_commit.author.name,
+        timestamp: run.head_commit.timestamp,
+        sha: run.head_sha
+      },
+      status: run.status,
+      conclusion: run.conclusion,
+      created: run.created_at,
+      updated: run.updated_at,
+      id: run.id
+    }))
+
+    startGroup(`All Runs (${runs.length})`)
+    console.log(runs)
+    endGroup()
+
+    const incompleteRuns = runs.filter(run => run.status !== 'completed')
+
+    startGroup(`Incomplete Runs (${incompleteRuns.length})`)
+    console.log(incompleteRuns)
+    endGroup()
+
+    if (incompleteRuns.length > 1) {
+      // const thisRun = incompleteRuns.find(run => run.commit.sha === sha)
+      // console.log(thisRun)
+      // await octokit.actions.cancelWorkflowRun({
+      //   owner,
+      //   repo,
+      //   run_id: thisRun.id
+      // })
+      setFailed('❌ Another run was already in process for this workflow and branch 💥')
+      process.exit(1)
+    }
+
+    console.log('✔ This was the only run for this workflow on this branch 🎉')
+  } catch (e) {
+    setFailed(`An error occurred: ${e.message || e}`)
+  }
+}
+
+run()
